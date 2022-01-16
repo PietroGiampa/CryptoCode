c1a1449689daa26c02db2c244dbdd8fde8b4b37e
PietroGiampa <giampapietro@gmail.com>
2022-01-15 21:51:08 -0500

    added comments to GetCurrency

--- GetCurrency.py
+++ GetCurrency.py
@@ -1,20 +1,29 @@
+###########################
+# GetCurrency.py Code     #
+#                         #
+# Pietro Giampa, Jan 2022 #
+###########################
+
 ## Libraries
 import numpy as np
 import pandas_datareader as pdr
 from datetime import datetime
 
+## Get Currency Data for a Single Day
 def GetCurrencySingleDay(currency, year, month, day):
     start = datetime(year,month,day)
     end = datetime(year,month,day)
     CC = pdr.DataReader(currency,'yahoo',start,end)
     return CC
 
+## Get Currency Data for the Past Year
 def GetCurrencyOneYear(currency):
     end = datetime.today()
     start = datetime(end.year-1, end.month, end.day)
     CC = pdr.DataReader(currency,'yahoo',start,end)
     return CC
 
+## Get Currency Data for the Past 5 Years
 def GetCurrencyHistory(currency):
     end = datetime.today()
     start = datetime(end.year-5, end.month, end.day)
