3314e02678ad1205ea80967a78c9583946ce8601
PietroGiampa <giampapietro@gmail.com>
2022-01-30 01:16:27 -0500

    Worked more on the daily report

--- DailyScript.py
+++ DailyScript.py
@@ -6,31 +6,30 @@
 
 ## Libraries
 import numpy as np
+import yagmail
 import matplotlib.pyplot as plt
 import pandas as pd
+import TrendPlotting as tp
+import Variables as currency
+import GetCurrency as gc
 
-# Adding Averages to the DataFrame
-# Need to specify origin DataFrame, original category, and # of days
+# Send Email with Reports
+# Need to specify sending email and password, receiver, body and filename
 # -----
-# crypto - Pandas DataFrame
-# category - String
-# days - Integer
-import smtplib, ssl
-
-def send_email_1(user, pwd, recipient, subject, body):
-    FROM = user
-    TO = recipient if isinstance(recipient, list) else [recipient]
-    SUBJECT = subject
-    TEXT = body
-    message = 'From: %s\nTo: %s\nSubject: %s\n\n%s' % (FROM, ", ".join(TO), SUBJECT, TEXT)
-    # SMTP_SSL Example
-    server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server_ssl.ehlo() # optional, called by login()
-    server_ssl.login(user, pwd)  
-    # ssl server doesn't support or need tls, so don't call server_ssl.starttls() 
-    server_ssl.sendmail(user, TO, message)
-    server_ssl.close()
+# user - String
+# psw - String
+# receiver - String
+# body - String
+# filename - String
+def send_email(user, psw, receiver, body, filename):
+    yag = yagmail.SMTP(user,psw)
+    yag.send(
+        to=receiver,
+        subject="Daily Crypto Investment Report",
+        contents=body, 
+        attachments=filename,
+    )
     print('successfully sent the mail')
-    
 
-send_email_1('giampapietro@gmail.com', 'qtbhvvwurtedunqb', 'danikaadam@gmail.com', 'Test Email from Python', 'Hi Gorgeous, I love you. \n Pietro')
+filename='Reports/Dogecoin_29_1_2022.png'
+send_email('giampapietro@gmail.com','qtbhvvwurtedunqb','danikaadam@gmail.com','Daily Crypto Investment Report',filename)
--- TrendPlotting.py
+++ TrendPlotting.py
@@ -12,7 +12,9 @@ from scipy.signal import find_peaks
 import GetCurrency as gc
 import Variables as currency
 import Simulations as mc
+import Analysis as analysis
 from datetime import datetime
+from datetime import timedelta
 
 ###########################
 # DataFrame               #
@@ -25,42 +27,89 @@ from datetime import datetime
 # 6. Volume               #
 ###########################
 
-## Select Crypto Currency
-select_crypto = input('Select Crypto Currency: ')
-crypto = currency.GetCurrencyTag(select_crypto)
+# Plot for Daily report for investments
+# Need to specify crypto
+# -----
+# select_crypto - String
+def GetOneYearOverview(select_crypto):
+    crypto = currency.GetCurrencyTag(select_crypto)
 
-## Pull Data From Last Week and Last Year
-DF_Year = gc.GetCurrencyOneYear(crypto)
-today = datetime.today()
-DF_Week = gc.GetCurrencyPreviousWeek(crypto, today.year, today.month, today.day)
+    ## Pull Data From Last Week and Last Year
+    DF_Year = gc.GetCurrencyOneYear(crypto)
+    today = datetime.today()
+    DF_Week = gc.GetCurrencyPreviousWeek(crypto, today.year, today.month, today.day)
 
-## Plot Style
-plt.style.use('dark_background')
-ax1 = plt.subplot2grid((2, 2), (0, 0), colspan=1)
-ax1.plot(DF_Year.index, DF_Year['Adj Close'], 'red')
-ax1.fill_between(DF_Year.index, DF_Year['Low'], DF_Year['High'], color='orange', alpha=0.5)
-ax1.set_ylabel('Adj Close')
-ax1.set_xlabel('Date')
-ax1.grid(True)
-ax2 = plt.subplot2grid((2, 2), (1, 0), colspan=1, sharex=ax1)
-ax2.plot(DF_Year.index, DF_Year['Volume'], 'green')
-ax2.set_ylabel('Volume')
-ax2.set_xlabel('Date')
-ax2.grid(True)
-ax3 = plt.subplot2grid((2, 2), (0, 1), rowspan=2)
-earnings = mc.SimPastYear(50000.0, 0.85, select_crypto)
-cs = ax3.contourf([1,2,3], [1,2,3], earnings, locator = ticker.LinearLocator(), cmap ="Greens")
-line_colors = ['black' for l in cs.levels]
-cp = ax3.contour([1,2,3], [1,2,3], earnings, colors=line_colors)
-ax3.clabel(cp, fontsize=10, colors=line_colors)
-ax3.set_xticks([1,2,3])
-ax3.set_yticks([1,2,3])
-ax3.grid(True)
-ax3.set_xlabel('Up Days - Invest')
-ax3.set_ylabel('Down Days - Sell')
-ax3.set_title('One Year MC Model')
-title_str = 'Crypto: '+select_crypto
-plt.suptitle(title_str)
-mng = plt.get_current_fig_manager()
-mng.resize(*mng.window.maxsize())
-plt.show()
+    ## Plot Style
+    plt.style.use('dark_background')
+    ax1 = plt.subplot2grid((2, 2), (0, 0), colspan=1)
+    ax1.plot(DF_Year.index, DF_Year['Adj Close'], 'red')
+    ax1.fill_between(DF_Year.index, DF_Year['Low'], DF_Year['High'], color='orange', alpha=0.5)
+    ax1.set_ylabel('Adj Close')
+    ax1.set_xlabel('Date')
+    ax1.grid(True)
+    ax2 = plt.subplot2grid((2, 2), (1, 0), colspan=1, sharex=ax1)
+    ax2.plot(DF_Year.index, DF_Year['Volume'], 'green')
+    ax2.set_ylabel('Volume')
+    ax2.set_xlabel('Date')
+    ax2.grid(True)
+    ax3 = plt.subplot2grid((2, 2), (0, 1), rowspan=2)
+    earnings = mc.SimPastYear(50000.0, 0.85, select_crypto)
+    cs = ax3.contourf([1,2,3], [1,2,3], earnings, locator = ticker.LinearLocator(), cmap ="Greens")
+    line_colors = ['black' for l in cs.levels]
+    cp = ax3.contour([1,2,3], [1,2,3], earnings, colors=line_colors)
+    ax3.clabel(cp, fontsize=10, colors=line_colors)
+    ax3.set_xticks([1,2,3])
+    ax3.set_yticks([1,2,3])
+    ax3.grid(True)
+    ax3.set_xlabel('Up Days - Invest')
+    ax3.set_ylabel('Down Days - Sell')
+    ax3.set_title('One Year MC Model')
+    title_str = 'Crypto: '+select_crypto
+    plt.suptitle(title_str)
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())    
+    figure = plt.gcf() # get current figure
+    figure.set_size_inches(12, 7)    
+    fig_name = 'Reports/'+select_crypto+'_'+str(today.day)+'_'+str(today.month)+'_'+str(today.year)+'.png'
+    plt.savefig(fig_name, dpi=100)
+
+
+#Plot for Daily report for investments
+# Need to specify crypto
+# -----
+# select_crypto - String 
+def GetCurrencyStatus():
+    names = currency.names
+    status = []
+    namex = []
+    today = datetime.today()
+    for n in range(len(names)):
+        namex.append(n)
+        print('Processing ... ', names[n])
+        crypto = currency.GetCurrencyTag(names[n])
+        DF1 = gc.GetCurrencyPreviousWeek(crypto, today.year, today.month, today.day)
+        DF1 = analysis.AddUPDOWN(DF1,'Volume')
+        setnum = int(DF1.size/DF1.columns.size)
+        if DF1['UPDOWN'][setnum-1]=='UP' and DF1['UPDOWN'][setnum-2]=='UP':
+            status.append(3)
+            GetOneYearOverview(names[n])
+        elif DF1['UPDOWN'][setnum-1]=='DOWN' and DF1['UPDOWN'][setnum-2]=='DOWN':
+             status.append(1)
+        else:
+            status.append(2)
+
+    fig, ax = plt.subplots()
+    ax.stem(namex, status)
+    ax.set_xticks(namex)
+    ax.set_yticks([1,2,3])
+    ax.set_yticklabels(['Sell','Hold','Invest'])
+    ax.set_xticklabels(names, rotation=90)
+    ax.grid(True)
+    ax.set_ylim(0.2,3.2)
+    figure = plt.gcf()
+    figure.set_size_inches(14, 8)
+    fig_name = 'Reports/CryptoStatus_'+str(today.day)+'_'+str(today.month)+'_'+str(today.year)+'.png'
+    plt.savefig(fig_name, dpi=100)
+    plt.show()
+    
+GetCurrencyStatus()
